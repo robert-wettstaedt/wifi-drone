72df78b907f0b44068e1f60b298aed30e2523cf7
Robert Wettstädt <robert.wett@gmail.com>
2017-06-20 11:51:13 +0200

    [rust-ffmpeg] Specifiy format and codec types

--- src/video/decoder.rs
+++ src/video/decoder.rs
@@ -3,11 +3,43 @@ extern crate ffmpeg;
 use super::Renderer;
 
 use self::ffmpeg::*;
-use std::thread;
+use std::ffi::{CString, CStr};
+use std::str::from_utf8_unchecked;
+use std::{ptr, thread};
+use std::path::Path;
 
 pub struct Decoder {
     codec: decoder::Video,
     context: format::context::Input,
+}
+
+pub fn input_with(path: &str) -> Result<format::context::Input, Error> {
+    let mut options = Dictionary::new();
+    options.set("iformat", "h264");
+    options.set("video_codec", "h264");
+
+    unsafe {
+        let format = CString::new("h264").unwrap();
+        let fmt = sys::av_find_input_format(format.as_ptr());
+
+        let mut ps   = ptr::null_mut();
+        let     path = CString::new(path).unwrap();
+        let mut opts = options.disown();
+        let     res  = sys::avformat_open_input(&mut ps, path.as_ptr(), fmt, &mut opts);
+
+        Dictionary::own(opts);
+
+        match res {
+            0 => {
+                match sys::avformat_find_stream_info(ps, ptr::null_mut()) {
+                    r if r >= 0 => Ok(format::context::Input::wrap(ps)),
+                    e           => Err(Error::from(e)),
+                }
+            }
+
+            e => Err(Error::from(e))
+        }
+    }
 }
 
 impl Decoder {
@@ -16,7 +48,7 @@ impl Decoder {
         format::register_all();
         println!("format::register_all");
 
-        let context = match format::input(&path) {
+        let context = match input_with(&path) {
             Ok(context) => context,
             Err(e) => panic!("Error opening h.264 stream with ffmpeg: {:?}", e),
         };

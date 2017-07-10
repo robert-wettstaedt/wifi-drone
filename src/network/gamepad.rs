0a9590952e10906520939859ddbd6f3ebbc237d5
Robert Wettstädt <robert.wett@gmail.com>
2017-07-10 19:56:19 +0200

    Add automatic take off keybind

--- src/network/gamepad.rs
+++ src/network/gamepad.rs
@@ -4,8 +4,11 @@ use glutin::{ElementState, VirtualKeyCode};
 
 use std::error::Error;
 use std::thread;
+use std::time::Duration;
 use std::net::UdpSocket;
 use std::sync::mpsc::Receiver;
+
+static UPDATE_INTERVAL: u64 = 50;
 
 pub type Callback = fn(command: &mut Command);
 
@@ -48,6 +51,8 @@ impl Gamepad {
         loop {
             let (state, virt_key_code) = self.keypress_rx.recv().unwrap();
 
+            (self.command_listener.callback)(&mut cmd);
+
             let value = if state == ElementState::Pressed { 127 } else { 0 };
 
             match virt_key_code {
@@ -59,12 +64,11 @@ impl Gamepad {
                 VirtualKeyCode::S => cmd.pitch = -value,
                 VirtualKeyCode::D => cmd.roll = value,
                 VirtualKeyCode::A => cmd.roll = -value,
+                VirtualKeyCode::Return => self.take_off(state == ElementState::Pressed, &mut cmd),
                 VirtualKeyCode::Space => cmd.toggle_mode(state == ElementState::Pressed),
                 VirtualKeyCode::Escape => cmd.mode = DroneMode::Abort,
                 _ => (),
             }
-
-            (self.command_listener.callback)(&mut cmd);
             println!("{:?}", cmd);
 
             self.write(&mut cmd);
@@ -76,6 +80,27 @@ impl Gamepad {
         match self.socket.send_to(&(cmd.as_array), format!("{}:{}", constants::DRONE_HOST, constants::DRONE_UDP_PORT)) {
             Ok(_) => debug!("Sent gamepad data"),
             Err(e) => panic!("Error writing gamepad data: {}", e.description()),
+        }
+    }
+
+    fn take_off(&mut self, is_toggling: bool, mut cmd: &mut Command) {
+        if !is_toggling {
+            return;
+        }
+
+        let duration = Duration::from_millis(UPDATE_INTERVAL);
+
+        cmd.mode = DroneMode::Normal;
+        self.write(&mut cmd);
+        thread::sleep(duration);
+        cmd.mode = DroneMode::TakingOff;
+
+        for i in 0..30 {
+            self.write(&mut cmd);
+            thread::sleep(duration);
+            cmd.throttle = (40 - i) as i8;
+
+            (self.command_listener.callback)(&mut cmd);
         }
     }
 }
